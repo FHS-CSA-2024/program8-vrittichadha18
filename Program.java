0890516867c33d4bb00d333f2da4196abf7e3925
vrittichadha18 <chadhavritti18@gmail.com>
2024-09-06 07:21:51 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,7 +1,39 @@
 //import stuff here
+import java.util.Scanner;
 
 //Your code here
-
+class Program8 {
+    public static void main(String[]args){
+        Scanner myScanner = new Scanner (System.in);
+        
+        int number1;
+        System.out.println("Enter number 1: ");
+        number1 = myScanner.nextInt();
+        
+        int number2;
+        System.out.println("Enter number 2: ");
+        number2 = myScanner.nextInt();
+        
+        int sum = number1 + number2;
+        int difference = number1 - number2;
+        int product = number1 * number2;
+        double average = (number1 + number2)/2.0;
+        int absoluteValue = Math.abs(difference);
+        int maximum;
+        int minimum;
+        
+        System.out.println();
+        
+        System.out.println("Original numbers are " + number1 + " and " + number2);
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+        System.out.printf("Average = %.1f%n", average);
+        System.out.println("Absolute Value = " + absoluteValue);
+        System.out.println("Maximum = " + maximum);
+        System.out.println("Minimum = " + minimum);
+    }
+}
 //Paste console output below:
 /*
 
